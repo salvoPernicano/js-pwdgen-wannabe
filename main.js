0c28c42f3d8dd54db942c07c86fd8a2ef01dfd45
salvoPernicano <salvo.pernicano@gmail.com>
2023-12-21 13:03:30 +0100

    feat prompt

---
+++ main.js
@@ -0,0 +1,5 @@
+let fName = prompt("qual è il tuo nome?", "salvo")
+let lName = prompt("qual è il tuo cognome?", "pernicano")
+let fColor = prompt("qual è il tuo colore preferito?", "rosso")
+
+document.writeln(fName+lName+fColor+"23")
